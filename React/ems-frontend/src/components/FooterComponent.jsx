58a42655924cd345e47a6f8a611772da18ab0fa4
raisulislam0 <mechrsl0@gmail.com>
2024-05-29 21:09:46 +0600

    Raisul's commit

---
+++ React/ems-frontend/src/components/FooterComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className = 'footer'>
+            <span> ALl rights reserved 2024 by Spring 2024</span>
+
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
